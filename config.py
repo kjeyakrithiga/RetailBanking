a18e27f423301b2c46f66ffa9efa0765b55b6c6c
kjeyakrithiga <34396931+kjeyakrithiga@users.noreply.github.com>
2020-06-23 08:53:39 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_string'
+    
+    MONGODB_SETTINGS = { 'db':'Retail_Bank' }
---
+++ main.py
@@ -0,0 +1,1 @@
+from Retail_Application import app
